50de4b2c69e0dc8dd0b45d5e0f28b88cabb6e4d0
Ethan <ethandsmith1@gmail.com>
2017-11-30 09:05:13 -0500

    voting weight

--- drdoogie.rb
+++ drdoogie.rb
@@ -34,7 +34,7 @@ def vote(trailing_vote, comment)
   upvote = trailing_vote.weight > 0
   downvote = trailing_vote.weight < 0
   unvote = trailing_vote.weight == 0
-  
+
   @config[:trails].each do |trail, options|
     next unless age <= options[:max_age]
     next if reply && !options[:enable_comments]
@@ -42,16 +42,16 @@ def vote(trailing_vote, comment)
     next if downvote && !options[:allow_downvote]
     next if (options[:skip_tags] & tags).any?
     next if !!options[:only_tags] && (options[:only_tags] & tags).any?
-    
+
     puts "#{trailing_vote.voter} voted for #{trailing_vote.author}/#{trailing_vote.permlink} at #{trailing_vote.weight / 100.0} %"
-    
+
     scale = options[:scale_votes].to_f / 100
-    weight = (trailing_vote.weight * scale).to_i
+    weight = scale.to_i
     
     @config[:voters].each do |voter|
       name, wif = voter.split(' ')
       next if comment.active_votes.map(&:voter).include? name
-      
+
       op = {
         type: :vote,
         voter: name,
@@ -59,7 +59,7 @@ def vote(trailing_vote, comment)
         permlink: trailing_vote.permlink,
         weight: weight
       }
-      
+
       tx = Radiator::Transaction.new(@options.dup.merge(wif: wif))
       tx.operations << op
       puts tx.process(true)
@@ -72,33 +72,33 @@ puts "Now trailing #{@config[:trails].keys.join(', ')} ..."
 loop do
   @api = Radiator::Api.new(@options.dup)
   @stream = Radiator::Stream.new(@options.dup)
-  
+
   mode = @config[:global][:mode].to_sym rescue :irreversible
-  
+
   begin
     @stream.operations(:vote, nil, mode) do |op|
       @backoff ||= 0.001
       next unless may_vote?(op)
       response = @api.get_content(op.author, op.permlink)
       comment = response.result
-      
+
       Thread.new do
         vote(op, comment)
       end
-      
+
       @backoff = nil
     end
   rescue => e
     m = e.message
-    
+
     if m =~ /undefined method `transactions' for nil:NilClass/ && mode == :head
       # Block hasn't reached the node yet.  Just retry with a small delay
       # without reporting an error.
-      
+
       sleep 0.2
     else
       puts "Pausing #{@backoff} :: Unable to stream on current node.  Error: #{e}"
-      
+
       sleep @backoff
       @backoff = [@backoff * 2, MAX_BACKOFF].min
     end
